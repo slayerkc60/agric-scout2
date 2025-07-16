0d76e57764d803884c881f5ad1aa8894a7f8c943
slayerkc60 <116925069+slayerkc60@users.noreply.github.com>
2025-07-16 11:35:54 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const signupForm = document.getElementById("signupForm");
+  if (signupForm) {
+    console.log(signupForm, "Signup form found");
+
+    signupForm.addEventListener("submit", function (event) {
+      event.preventDefault();
+
+      const firstName = document.getElementById("firstName").value.trim();
+      const lastName = document.getElementById("lastName").value.trim();
+      const email = document.getElementById("email").value.trim();
+      const password = document.getElementById("password").value.trim();
+
+      fetch("https://scout-m4ru.onrender.com/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          first_name: firstName,
+          last_name: lastName,
+          email: email,
+          password: password
+        })
+      })
+      
+      .then(response => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then(data => {
+        console.log("success:", data);
+        localStorage.setItem("email", email);
+      })
+
+      .catch(error => {
+        console.error("There was a problem with the fetch:", error);
+      });
+    });
+  }
+});
+
+
+
+
+
