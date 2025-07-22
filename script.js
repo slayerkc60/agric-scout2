cff9b016ffda404fcdcf4ba344f7488c034cc71a
slayerkc60 <116925069+slayerkc60@users.noreply.github.com>
2025-07-22 09:22:12 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ document.addEventListener("DOMContentLoaded", () => {
       const email = document.getElementById("email").value.trim();
       const password = document.getElementById("password").value.trim();
 
-      fetch("https://scout-m4ru.onrender.com/register", {
+      fetch("https://scout-m4ru.onrender.com/auth/register", {
 
         method: "POST",
         headers: {
@@ -60,7 +60,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
       console.log("Attempting login with:", email, password);
 
-      fetch("https://scout-m4ru.onrender.com/login", {
+      fetch("https://scout-m4ru.onrender.com/auth/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
